88137176a8983cdcb815ddcf99980060f1804198
UFOintelligence <ramonbrito@gmail.com>
2024-03-13 13:01:28 -0500

    programa web para manejar archivos

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,16 @@
+<x-admin-layout>
+    <p>Dignissimos fugit ra numquam expedita ipsam, ratione
+        molestias facilis rem harum. Provident laudantium neque nisi quisLorem
+        . Provident laudantium neque nisi quis
+        Dignissimos fugit ra numquam expedita ipsam, ratione
+        molestias facilis rem harum. Provident laudantium neque nisi quisLorem
+        . Provident laudantium neque nisi quis
+        Dignissimos fugit ra numquam expedita ipsam, ratione
+        molestias facilis rem harum. Provident laudantium neque nisi quisLorem
+        . Provident laudantium neque nisi quis
+        Dignissimos fugit ra numquam expedita ipsam, ratione
+        molestias facilis rem harum. Provident laudantium neque nisi quisLorem
+        . Provident laudantium neque nisi quis.</p>
+</x-admin-layout>
+
+
---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+       <!-- fontawesome -->
+        <script src="https://kit.fontawesome.com/83af161d9e.js" crossorigin="anonymous"></script>
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        <!-- Styles -->
+        @livewireStyles
+    </head>
+    <body class="font-sans antialiased ":class="{'overflow-hidden': open}"
+    x-data="{open: false}">
+
+     @include('layouts.includes.admin.nav')
+     @include('layouts.includes.admin.aside')
+
+
+
+
+          <div class="p-4 sm:ml-64">
+             <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
+
+
+
+                {{$slot }}
+             </div>
+
+
+          </div>
+
+          <div x-show="open"
+          class="bg-gray-900/50 dark:bg-gray-900/80
+          fixed inset-0 z-30 sm:hidden"></div>
+
+        @stack('modals')
+
+        @livewireScripts
+    </body>
+</html>
